e63717718ca6048d7965f8f5348090930b7f6913
Elad Daniel <110186598+elad540@users.noreply.github.com>
2023-09-06 15:23:11 +0300

    Update passgenerator.py

--- PasswordGenerator/passgenerator.py
+++ PasswordGenerator/passgenerator.py
@@ -1,21 +1,45 @@
-# This script will generate a random password for the user.
 import random
+import string
 
+def weak_password_generator(length):
 
-def generate_password(length):
-    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-"
-    password = ""
-    for i in range(length):
-        password = password + random.choice(characters)
-    return password
+  characters = string.ascii_lowercase
+  password = "".join(random.choice(characters) for i in range(length))
+  return password
 
+def strong_password_generator(length):
 
-def main():
-    print("Welcome to the Passwords Generator")
-    password_length = int(input("Enter the length of the password you want: "))
-    password = generate_password(password_length)
-    print("Your new password is:", password)
+  characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-"
+  password = ""
+  for i in range(length):
+    password = password + random.choice(characters)
+  return password
 
+def select_password_generator():
+
+  print("Please choose which kind of Password you would like to have:")
+  print("1. Weak Password")
+  print("2. Strong Password")
+  choice = input("Your choice: ")
+  if choice == "1":
+    return weak_password_generator
+  elif choice == "2":
+    return strong_password_generator
+  else:
+    print("Wrong typo.")
+    return None
+
+def generate_password():
+
+  password_generator = select_password_generator()
+  if password_generator is not None:
+    password = password_generator(8)
+    print("Your password is:", password)
+    file = open("password.txt", "w")
+    file.write(password)
+    file.close()
+  else:
+    print("Can't create a password.")
 
 if __name__ == "__main__":
-    main()
+  generate_password()
